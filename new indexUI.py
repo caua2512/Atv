e8fb8874af8b450c8843723f735cb380c207c995
caua2512 <130388387+caua2512@users.noreply.github.com>
2023-10-16 10:29:26 -0300

    Create new indexUI.py

---
+++ new indexUI.py
@@ -0,0 +1,10 @@
+from ManterClienteUI import ManterClienteUI
+
+class IndexUI:
+    @classmethod
+    def main(cls):
+      #InicioUI.main()
+      #EquacaoUI.main()
+      ManterClienteUI.main()
+
+IndexUI.main()

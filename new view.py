4c1b0b6dd729622097e07223e1b43ba3fed644e2
caua2512 <130388387+caua2512@users.noreply.github.com>
2023-10-20 11:52:03 -0300

    Update new view.py

--- new view.py
+++ new view.py
@@ -1,4 +1,6 @@
 from models.cliente import Cliente, NCliente
+from models.serviço import Servico, NServico
+from models.agenda import Agenda, NAgenda
 
 class View:
   @classmethod
@@ -16,3 +18,33 @@ class View:
   def cliente_excluir(cls, id):
     cliente = Cliente(id," ", " ", " ")
     NCliente.excluir(cliente)
+  @classmethod
+  def servico_inserir(cls, Descrição, valor, Duração):
+    serviço = Servico(0, Descrição, valor, Duração)
+    NServico.inserir(serviço)
+  @classmethod
+  def servico_listar(cls):
+    return NServico.listar()
+  @classmethod
+  def servico_atualizar(cls, id, Descrição, valor, Duração):
+    servico = Servico(id, Descrição, valor, Duração)
+    NServico.atualizar(servico)
+  @classmethod
+  def servico_excluir(cls, id):
+    servico = Servico(id," ", " ", " ")
+    NServico.excluir(servico)
+  @classmethod
+  def agenda_inserir(cls, Descrição, valor, Duração):
+    agenda = Agenda(0, Descrição, valor, Duração)
+    NAgenda.inserir(agenda)
+  @classmethod
+  def agenda_listar(cls):
+    return NAgenda.listar()
+  @classmethod
+  def agenda_atualizar(cls, id, Descrição, valor, Duração):
+    agenda = Agenda(id, Descrição, valor, Duração)
+    NAgenda.atualizar(agenda)
+  @classmethod
+  def agenda_excluir(cls, id):
+    agenda = Agenda(id," ", " ", " ", " ")
+    NAgenda.excluir(agenda)
